3488277d1608044d5e18c0f42b80dd633d6d0ccb
Raghav-2104 <mundhararaghav16@gmail.com>
2024-02-15 22:34:54 +0530

    AIDS CA

---
+++ MPL/expt5/lib/gesture.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class GesturePage extends StatefulWidget {
+  const GesturePage({super.key});
+
+  @override
+  State<GesturePage> createState() => _GesturePageState();
+}
+
+class _GesturePageState extends State<GesturePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text('Gesture Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Tap'),
+                  ),
+                );
+              },
+              onDoubleTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Double Tap'),
+                  ),
+                );
+              },
+              onLongPress: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Long Press'),
+                  ),
+                );
+              },
+              child: Container(
+                height: 150.0,
+                width: 150.0,
+                color: Colors.teal,
+                child: const Center(
+                  child: Text('Tap Me'),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed:(){
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple,
+                foregroundColor: Colors.black
+              ),
+              child:const Text('Go to Previous Screen'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ MPL/expt5/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:expt5/gesture.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        // '/': (context) => const MyHomePage(),
+        '/gesture': (context) => const GesturePage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      appBar: AppBar(
+        title: const Text('Navigation Example'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: ButtonBar(
+          alignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/gesture');
+              },
+              child: const Text('Go to Gesture Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Unknown route. Please try again.'),
+                  ),
+                );
+              },
+              child: const Text('Open route'),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
